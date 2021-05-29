c4ee38296a901766904463602333bad014addc0b
nitrikto <sandezster@gmail.com>
2021-05-29 15:14:37 -0300

    modified

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
-provider "aws"{
-	region = "us-east-1"
-  default_tags {
-    tags = {
+provider "aws" {}
+resource "aws_instance" "Flugel" {
+  ami           = "ami-0d5eff06f840b45e9"
+  instance_type = "t2.micro" 
+  tags = {
       Name = "Flugel"
       Owner = "InfraTeam"
     }
-   }
-}
-
-resource "aws_instance" "Flugel" {
-  ami           = "ami-0d5eff06f840b45e9"
-  instance_type = "t2.micro"
   }
 
 resource "aws_s3_bucket" "flugelb" {
   bucket = "flugelbucket"
   acl    = "private"
+  tags = {
+      Name = "Flugel"
+      Owner = "InfraTeam"
+    }
 }
