7b7c7c1794f31488711de921bcda74af33f6d3c6
nitrikto <sandezster@gmail.com>
2021-05-29 12:38:39 -0300

    only main

--- ec2.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "Flugel" {
-  instance_type = "t2.micro"
-  ami = "ami-0d5d9d301c853a04a"
-}
--- s3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "FlugelBucket"
-  acl    = "private"
-}
