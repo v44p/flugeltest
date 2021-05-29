dfe47cec770be5a0f5712506a313432cc8bff26a
nitrikto <sandezster@gmail.com>
2021-05-29 14:40:21 -0300

    adasd

--- main.tf
+++ main.tf
@@ -8,14 +8,7 @@ variable "my_secret_key" {
   default = "no_secret_key_value_found"
 }
  
-output "access_key_is" {
-  value = var.my_access_key
-}
- 
-output "secret_key_is" {
-  value = var.my_secret_key
-}
- 
+
 provider "aws" {
 	region = "us-east-1"
 	access_key = var.my_access_key
