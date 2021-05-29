eb282440a4f3f9af864726a8ef5db43539bf2011
nitrikto <sandezster@gmail.com>
2021-05-29 16:32:48 -0300

    ass

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ resource "aws_instance" "flugel" {
   ami           = "ami-0d5eff06f840b45e9"
   instance_type = "t2.micro"
   tags = {
-      Name = "Flugel"
-      Owner = "InfraTeam"
-    }
+    Name = "Flugel"
+    Owner = "InfraTeam"
+  }
 }
   
 resource "aws_s3_bucket" "flugelb" {
   bucket = "flugelbucket"
   acl    = "private"
   tags = {
-      Name = "Flugel"
-      Owner = "InfraTeam"
-    }
+    Name = "Flugel"
+    Owner = "InfraTeam"
+  }
 }
