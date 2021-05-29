929c1222e75d9a2ca61ec76668ded487a0ed035b
nitrikto <sandezster@gmail.com>
2021-05-29 13:07:51 -0300

    modified ami aws

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 resource "aws_instance" "Flugel" {
   instance_type = "t2.micro"
-  ami = "ami-0d5d9d301c853a04a"
+  ami = "ami-09d19e919d57453f8"
 }
 
 resource "aws_s3_bucket" "b" {
